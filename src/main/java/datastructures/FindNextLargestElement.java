60e4b78370796e2bd9b7863dab4a81de9ad98f73
PEROOT\vkahnze <zeeshanahmed.khan@pearson.com>
2020-01-07 13:47:24 +0530

    add-next-greater-number-in-array-solution

--- src/main/java/datastructures/FindNextLargestElement.java
+++ src/main/java/datastructures/FindNextLargestElement.java
@@ -1,12 +1,23 @@
 package datastructures;
 
+import java.util.Stack;
+
 public class FindNextLargestElement {
 
 	public static void main(String[] args) {
-		int a[] = { 1, 2, 3, 4, 5 };
-		int n = a.length;
-		for (int i = 0; i < n; i++) {
-			
+		int a[] = {13, 7, 6, 12};
+
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(a[0]);
+
+		for (int i = 1; i < a.length; i++) {
+			while (!stack.isEmpty() && stack.peek() < a[i]) {
+				System.out.println(stack.pop() + " --->>> " + a[i]);
+			}
+			stack.push(a[i]);
+		}
+		while (!stack.isEmpty()) {
+			System.out.println(stack.pop()+ " --->>> -1");
 		}
 	}
 
